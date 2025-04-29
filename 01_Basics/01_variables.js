b5aa4cda57cbd25943dd4368bbc2f7ac40a2b187
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-04-29 19:16:15 +0000

    Variables

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12336567
+var accountPassword = "234324432"
+let accountEamil = "shoaibalam@gmail.com"
+accountCity = "zhob"
+let accountstate;
+
+//accountId = 1212121
+//accountEamil = "sarwan@gmail.com"
+// accountPassword = "2343223"
+// accountCity = "quetta"
+
+console.log(accountId);
+
+console.table([accountId, accountEamil, accountPassword, accountCity, accountstate])
