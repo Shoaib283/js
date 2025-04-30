0598a44d0ba8c1ddffa6f62319bb834708491115
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-04-30 07:00:22 +0000

    datatype-stake and heap

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,34 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stake (primitive),  heap (non-primitive)
+
+let myyoutubename = "shoaib@gmail.com"
+
+let anothername = myyoutubename
+anothername = "alam"
+
+console.log(myyoutubename);
+console.log(anothername);
+
+let userone ={
+
+    name : "shoaib",
+    age : 22
+}
+
+let usertwo = userone
+
+usertwo.email = "shoaib@gmail.com"
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
