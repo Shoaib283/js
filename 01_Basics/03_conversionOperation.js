260893c022623562f577facb41171788ebc8a363
Shoaib Alam <shoaibalammandokhail00@gmail.com>
2025-04-30 04:59:09 +0000

    conversion od datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+
+//console.log(valueInNumber)
+
+let islogedIn = "shoaib"
+
+let boleanisloggedin = Boolean(islogedIn)
+//console.log(boleanisloggedin)
+
+let someNumber = 88
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
